7bcdd5ab124963ef8615ad3fd1c9daa4a947fdd9
jmilapunk <javier_mila@hotmail.com>
2025-05-29 19:12:16 -0700

    Extendiendo Timeout para RetrofitClientFlask

--- SpotifyLikedSongsGenreSorter/app/src/main/java/com/example/spotifylikedsongsgenresorter/api/RetrofitClientFlask.kt
+++ SpotifyLikedSongsGenreSorter/app/src/main/java/com/example/spotifylikedsongsgenresorter/api/RetrofitClientFlask.kt
@@ -1,14 +1,24 @@
 package com.example.spotifylikedsongsgenresorter.api
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClientFlask {
-    private const val BASE_URL = "https://likedgenresorter.com" // Tu backend
+    private const val BASE_URL = "https://likedgenresorter.com" // o tu backend real
+
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)   // antes 30
+        .readTimeout(120, TimeUnit.SECONDS)     // antes 60
+        .writeTimeout(120, TimeUnit.SECONDS)    // antes 60
+        .build()
+
     val instance: FlaskLikedSongsService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(FlaskLikedSongsService::class.java)
     }
